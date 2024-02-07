94100d49fddc25839c18f4667324aace877a7446
Ravi Singh <ravisinghrajputana9@gmail.com>
2024-02-07 20:18:24 +0530

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -208,6 +208,24 @@
   });
 
   /**
+   * Home Slider
+   */
+  new Swiper('.home-slider', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
    * Testimonials slider
    */
   new Swiper('.testimonials-slider', {
