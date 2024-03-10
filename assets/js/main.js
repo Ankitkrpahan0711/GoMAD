9ea5015b37ea6d15ef43cbad25bb3dfbd62c5ece
Ravi Singh <ravisinghrajputana9@gmail.com>
2024-03-10 14:53:48 +0530

    slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -243,6 +243,7 @@
     }
   });
 
+
   /**
    * Animation on scroll
    */
@@ -261,3 +262,23 @@
   new PureCounter();
 
 })()
+  /*
+  *background slider
+  */
+  let slideIndex = 0;
+  const slides = document.getElementsByClassName("slide");
+  
+  function showSlides() {
+      for (let i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";  
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {
+          slideIndex = 1;
+      }    
+      slides[slideIndex - 1].style.display = "block";  
+      setTimeout(showSlides, 5000); // Change image every 2 seconds
+  }
+  
+  showSlides();
+  
